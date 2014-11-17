ee992dbfed18656ef6c09fd2a8e342791660a0af
gabrie30 <jgabriels30@gmail.com>
2014-11-16 21:09:08 -0500

    Update 4e_minutes_in_a_year.rb

---
+++ 1_week/4e_minutes_in_a_year.rb
@@ -0,0 +1,10 @@
+# Write a Ruby program that tells you how many minutes there are in a year (do not bother right now about leap years etc.).
+
+def mins_a_year
+	days = 365
+	hours = days * 24
+	minutes = hours * 60
+	p "There are #{minutes} minutes in a year"
+end
+
+mins_a_year
