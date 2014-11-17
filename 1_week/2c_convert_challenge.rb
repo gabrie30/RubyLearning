7098e3afc1fbc32957714899cfbf49df5b665eb9
gabrie30 <jgabriels30@gmail.com>
2014-11-17 12:03:13 -0500

    initial commit

--- 1_week/2c_convert_challenge.rb
+++ 1_week/2c_convert_challenge.rb
@@ -1,72 +1,126 @@
 def ask_user
-  puts "
-  Please select your initial temerature you are converting from:
-   a. Fahrenheit
-   b. Celsius
-   c. Kelvin
-   d. Rankine
+  puts %Q{Please select your initial temperature you are converting from:
+     a. Fahrenheit
+     b. Celsius
+     c. Kelvin
+     d. Rankine}
 
-   "
-   choice = gets.chomp
+  choice = gets.chomp.downcase
 
-   puts "How many degrees would you like to convert: "
-   degrees = gets.to_i
+  puts "How many degrees would you like to convert: "
+  degrees = gets.to_i
 
-   if choice == "a"
-    far(degrees)
-   elsif choice == "b"
-    cel(degrees)
-   elsif choice == "c"
-    kel(degrees)
-   elsif choice == "d"
-    ran(degrees)
+  puts %Q{What would you like to convert into?
+     a. Fahrenheit
+     b. Celsius
+     c. Kelvin
+     d. Rankine}
+
+  convert_to = gets.chomp.downcase
+
+# you will enter the function of the initial unit to solve for the desired answer
+# for example if a user wants to convert Kelvin 'kel' to Celsius 'cel' -- you will enter the kel method
+
+   if convert_to == "a"
+    converting_to = "Fahrenheit"
+   elsif convert_to == "b"
+    converting_to = "Celsius"
+   elsif convert_to == "c"
+    converting_to = "Kelvin"
+   elsif convert_to == "d"
+    converting_to = "Rankine"
+   else
+      puts "That's not an option try again"
    end
 
+  if choice == "a"
+    initial_unit = "Fahrenheit"
+    far(degrees,converting_to,initial_unit)
+  elsif choice == "b"
+    initial_unit = "Celsius"
+    cel(degrees,converting_to,initial_unit)
+  elsif choice == "c"
+    initial_unit = "Kelvin"
+    kel(degrees,converting_to,initial_unit)
+  elsif choice == "d"
+    initial_unit = "Rankine"
+    ran(degrees,converting_to,initial_unit)
+  else
+    puts "That's not an option try again"
+  end
+
 end
 
-def far(deg)
-  cel = (deg - 32) * 5 / 9
-  kel = (deg + 459.67) * 5 / 9
-  ran = deg + 459.67
-  far = deg
-  far_n = "Fahrenheit"
-  output(deg,far_n,cel,kel,ran,far)
+# I could make the function calls more DRY, because the last argument is the only one that changes,
+# however I would still need to create another variable for each if/else so I left it like it is
+# will think about how I can solve this problem
+
+def far(deg,converting_to,initial_unit)
+  if converting_to == "Celsius"
+    cel = (deg - 32) * 5 / 9
+    output(deg,initial_unit, converting_to, cel)
+  elsif converting_to == "Kelvin"
+    kel = (deg + 459.67) * 5 / 9
+    output(deg,initial_unit, converting_to, kel)
+  elsif converting_to == "Rankine"
+    ran = deg + 459.67
+    output(deg,initial_unit, converting_to, ran)
+  elsif converting_to == "Fahrenheit"  
+    far = deg
+    output(deg,initial_unit, converting_to, far)
+  end
 end
 
-def cel(deg)
-  kel = deg + 273.15
-  ran = (deg + 273.15) * 9/5
-  far =  deg *  9 / 5 + 32
-  cel = deg
-  cel_n = "Celsius"
-  output(deg,cel_n,cel,kel,ran,far)
+def cel(deg,converting_to,initial_unit)
+  if converting_to == "Kelvin"
+    kel = deg + 273.15
+    output(deg,initial_unit, converting_to, kel)
+  elsif converting_to == "Rankine"
+    ran = (deg + 273.15) * 9/5
+    output(deg,initial_unit, converting_to, ran)
+  elsif converting_to == "Fahrenheit" 
+    far =  deg *  9 / 5 + 32
+    output(deg,initial_unit, converting_to, far)
+  elsif converting_to == "Celsius"
+    cel = deg
+    output(deg,initial_unit, converting_to, cel)
+  end
 end
 
-def kel(deg)
-  ran = deg * 9 / 5
-  far = deg * 9 / 5 - 459.67
-  cel = deg - 273.15
-  kel = deg
-  kel_n = "Kelvin"
-  output(deg, kel_n,cel,kel,ran,far)
+def kel(deg,converting_to,initial_unit)
+  if converting_to == "Rankine"
+    ran = deg * 9 / 5
+    output(deg,initial_unit, converting_to, ran)
+  elsif converting_to == "Fahrenheit"
+    far = deg * 9 / 5 - 459.67
+    output(deg,initial_unit, converting_to, far)
+  elsif converting_to == "Celsius"
+    cel = deg - 273.15
+    output(deg,initial_unit, converting_to, cel)
+  elsif converting_to == "Kelvin"
+    kel = deg
+    output(deg,initial_unit, converting_to, kel)
+  end
 end
 
-def ran(deg)
-  far = deg - 459.67
-  cel =  (deg - 491.67) * 5 / 9
-  kel = deg * 5 / 9
-  ran = deg
-  ran_n = "Rankine"
-  output(deg,ran_n,cel,kel,ran,far)
+def ran(deg,converting_to,initial_unit)
+  if converting_to == "Fahrenheit"
+    far = deg - 459.67
+    output(deg,initial_unit, converting_to, far)
+  elsif converting_to == "Celsius"
+    cel =  (deg - 491.67) * 5 / 9
+    output(deg,initial_unit, converting_to, cel)
+  elsif converting_to == "Kelvin"  
+    kel = deg * 5 / 9
+    output(deg,initial_unit, converting_to, kel)
+  elsif converting_to == "Rankine"
+    ran = deg
+    output(deg,initial_unit, converting_to, ran)
+  end
 end
 
-def output(initial_temp, initial_unit, cel, kel, ran, far)
-  p "#{initial_temp} degrees #{initial_unit} converts to"
-  p "######################################################"
-  p "#{cel} degrees Celsius"
-  p "#{kel} degrees Kelvin"
-  p "#{ran} degrees Rankine"
-  p "#{far} degrees Fahrenheit"
+def output(deg, initial_unit, converting_to, answer)
+ puts "#{deg} degrees #{initial_unit} converts to #{answer} degrees #{converting_to}"
 end
 
 ask_user
