7d09093164d8f70ea1b07893091a67f0bfe59e2d
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2014-11-17 01:42:55 +0000

    inital commit

--- 1_week/1c_year_months.rb
+++ 1_week/1c_year_months.rb
@@ -1,6 +1,4 @@
-#
-#  what we have learned so far: I wonder if anyone could have a crack at working out the age in years and months form. For example “I’m 32 years and 6 months old.” Use the following values for age_in_seconds:
-#  
+##
 ##
 def seconds_to_years(seconds)
   minutes = seconds / 60.000
