ca75066adfcc0a2e0560b1930d0013bf5a460703
gabrie30 <jgabriels30@gmail.com>
2014-11-17 19:33:59 -0500

    initial commit

--- 1_week/1c_year_months.rb
+++ 1_week/1c_year_months.rb
@@ -1,5 +1,3 @@
-##
-##
 def seconds_to_years(seconds)
   minutes = seconds / 60.000
   hours = minutes / 60
@@ -8,13 +6,13 @@ def seconds_to_years(seconds)
   months_remaining = (years - years.floor) * 12
   weeks_remaining = (months_remaining - months_remaining.floor) * 4
   days_remaining = (weeks_remaining - weeks_remaining.floor) * 7
-  hours_remaining = (days_remainging - days_remaining.floor) * 24
+  hours_remaining = (days_remaining - days_remaining.floor) * 24
   minutes_remaining = (hours_remaining - hours_remaining.floor) * 60
-  seconds_remaining = (minutes_remaining - mintues_remaining.floor) * 60
+  seconds_remaining = (minutes_remaining - minutes_remaining.floor) * 60
   
   p "I'm #{years.floor} years #{months_remaining.floor} months #{weeks_remaining.floor} weeks #{days_remaining.floor} days #{hours_remaining.floor} hours #{minutes_remaining.floor} minutes #{seconds_remaining.floor} seconds old"
 end
-
+ 
 seconds_to_years(979000000)
 seconds_to_years(2158493738)
 seconds_to_years(246144023)
