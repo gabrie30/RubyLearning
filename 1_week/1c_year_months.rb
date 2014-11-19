4ff15a66ece52072e70c861ff95c5743cb89367a
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2014-11-19 04:31:53 +0000

    Redesign of year and months challenge
    Now more code, but better documented
    - Add application guard
    - Our method is now broken down to individual methods to compartmentalize
    the calculations and identify what they do
    - Testing helps us to refactor our code
    - Return is no longer nil, and reporting is done outside of our methods

--- 1_week/1c_year_months.rb
+++ 1_week/1c_year_months.rb
@@ -1,23 +1,74 @@
-#this will take seconds and convert it to years, days, hours, minutes, seconds
-
-def seconds_converted(seconds)
-	remaining_seconds = seconds % 60
-	minutes = seconds / 60
-	remaining_minutes = minutes % 60
-	hours = minutes / 60
-	remaining_hours = hours % 24 # know: 1 % 24 = 1
-	days = hours / 24	     # now: days = 1 / 24 => 0 ; this continues
-	remaining_days = days % 365 
-	years = days / 365
-	remaining_years = years % 365
-
-puts "#{seconds} seconds is equal to #{remaining_years} years, #{remaining_days} days, #{remaining_hours} hours #{remaining_minutes} minutes #{remaining_seconds} seconds}"
+# this will take seconds and convert it to years, days, hours, minutes, seconds
+# doctest: Seconds to minutes when given 60 will return 1
+# >> seconds_to_minutes 60
+# => 1
+# doctest: 2 minutes worth of seconds should be 2
+# >> seconds_to_minutes 120
+# => 2
+def seconds_to_minutes(seconds)
+  seconds / 60
 end
-seconds_converted(3600) 
- 
-seconds_to_years(979000000)
-seconds_to_years(2158493738)
-seconds_to_years(246144023)
-seconds_to_years(1270166272)
-seconds_to_years(1025600095)
+# doctest: minutes to hours should return 1 when given 60
+# >> minutes_to_hours 60
+# => 1
+# doctest: minutes to hours should return 2 when given 120
+# >> minutes_to_hours 120
+# => 2
+def minutes_to_hours(minutes)
+  minutes / 60
+end
 
+# doctest: hours to days should return 1 when given 24
+# >> hours_to_days 24
+# => 1
+def hours_to_days(hours)
+  hours / 24
+end
+# doctest: day to years should return 1 when given 365
+# >> days_to_years 365
+# => 1
+def days_to_years(days)
+  days / 365
+end
+
+# doctest: months_in_year
+# >> months_in_year
+# => 12
+def months_in_year
+  12
+end
+
+# Takes an argument in seconds, and returns an 2 element array, the first
+# (index 0) being year, the seconds being months.
+# doctest: seconds_converted_to_years_months
+# >> seconds_converted_to_years_months(60 * 60 * 24 * 365)
+# => [1, 0]
+# rubydoctest: seconds_converted_to_years_months
+# >> seconds_converted_to_years_months(60 * 60 * 24 * 365 * 2)
+# => [2, 0]
+# rubydoctest: seconds_converted_to_years_months
+# >> seconds_converted_to_years_months(60 * 60 * 24 * 365 * 2.5)
+# => [2, 6]
+# rubydoctest: seconds_converted_to_years_months
+# >> seconds_converted_to_years_months(60 * 60 * 24 * 365 * 3.15)
+# => [3, 1]
+def seconds_converted_to_years_months(seconds)
+  years = days_to_years(hours_to_days(minutes_to_hours(
+    seconds_to_minutes(seconds))))
+  months = years % 1 * 12
+  [years.floor, months.floor]
+end
+
+if __FILE__ == $PROGRAM_NAME
+  seconds = 979_000_000
+  remaining_years, remaining_months = seconds_converted_to_years_months(seconds)
+  puts "#{seconds} is #{remaining_years} years and #{remaining_months} months."
+  puts "#{seconds} seconds is equal to #{remaining_years} years, #{remaining_days} days, #{remaining_hours} hours #{remaining_minutes} minutes #{remaining_seconds} seconds}"
+  seconds_converted(3600)
+
+  seconds_to_years(979_000_000)
+  seconds_to_years(2_158_493_738)
+  seconds_to_years(246_144_023)
+  seconds_to_years(1_270_166_272)
+  seconds_to_years(1_025_600_095)
+end
