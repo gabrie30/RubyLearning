992d807d8c93310b11f72d24f7498db45f51e5d3
jgabez <jgabriels30@gmail.com>
2014-11-22 19:18:47 -0500

    changed to each_with_index

--- 2_week/1e_split_a_string.rb
+++ 2_week/1e_split_a_string.rb
@@ -1,12 +1,6 @@
-#
 s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
 s = s.split("\n")
-i = 1
-while i < s.length
 
-	s.each do |line|
-		puts "Line #{i}:  #{line}"
-		i += 1
-	end
-
-end
+s.each_with_index do |line, index|
+  puts "Line #{index + 1}:  #{line}"
+  end
