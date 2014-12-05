bb020be65390fe2a974fae3faaed7ff09c755656
Victor Goff <keeperotphones@gmail.com>
2014-12-04 20:10:25 -0500

    Ignore Files, Swap Files and Text File
    The file .gitignore lets us ignore files that may be needed for our
    project but that are either generated or otherwise not needed to be
    public (at least public to the repository).
    
    The ignored text.txt file is due to two uses, one for exercise dealing
    with document statistics and the other is string replacement.  The same
    filename is often used for both, specifically 'text.txt'.  But we also
    require that the programs don't hardcode the name.
    
    The ignore file now has the vim swp files ignored as well.
    
    This commit (in bad form) also has the document stats program added in.

---
+++ 3_week/7e_document_stats.rb
@@ -0,0 +1,18 @@
+file = File.open(ARGV[0], "r")
+lines = file.readlines
+ 
+total_characters_excluding_whitespace = lines.join.gsub(" ", "").delete("\n").split("").length
+total_characters_including_whitespace = lines.join.delete("\n").split("").length
+total_words = lines.join.split(" ").length
+total_sentences = lines.join.split(/\.|\?|\!/).length
+total_paragraphs = lines.join.split(/\n\n/).length
+ 
+puts "Statistics for the file: "
+puts "There are #{lines.length} lines"
+puts "There are #{total_characters_excluding_whitespace} characters, excluding whitespace and newlines"
+puts "There are #{total_characters_including_whitespace} characters, including whitespace, but excluding newlines"
+puts "There are #{total_words} total words"
+puts "There are #{total_sentences} total sentences"
+puts "There are #{total_paragraphs} total paragraphs"
+puts "There are on average #{total_words / total_sentences} words per sentence"
+puts "There are on average #{total_sentences / total_paragraphs} sentences per paragraph"
