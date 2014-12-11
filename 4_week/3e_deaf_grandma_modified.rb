91824cc23f416afd5b8c52ee5352eaa31c6d687b
RubyLearning.org Pair Programming <keeperotphones@gmail.com>
2014-12-11 00:44:05 +0000

    Refactored DeafGranny
    Quite a few helper methods... still missing private designation

--- 4_week/3e_deaf_grandma_modified.rb
+++ 4_week/3e_deaf_grandma_modified.rb
@@ -1,38 +1,45 @@
 class Grandma
-
-  def initialize(name, count=0, deafness=3)
+  def initialize(name, count = 0, deafness = 3)
     @name = name
     @count = count
     @deafness = deafness
   end
 
   def done_talking?
-    @count == @deafness ? true : false
+    @count == @deafness
   end
 
   def count(you_ask)
-    if you_ask == "BYE"
-      @count += 1
-    else
-      @count = 0
-    end
+    you_ask == 'BYE' ?  @count += 1 : @count = 0
   end
 
   def says(you_ask)
-    if you_ask != "BYE" && you_ask == you_ask.upcase && you_ask != ""
-      "#{@name} says: OH NO! Not since #{rand(1930..1950)}"
-    else
+    heard?(you_ask) ?
+      "#{@name} says: OH NO! Not since #{rand(1930..1950)}" :
       "#{@name} says: WHAT?! SONNY SPEAKUP I CAN'T HEAR YOU!"
-    end
   end
 
-end
+  def heard?(you_ask)
+    yelled?(you_ask) unless bye?(you_ask) || silence?(you_ask)
+  end
 
+  def silence?(statement)
+    statement.empty?
+  end
+
+  def yelled?(statement)
+    statement == statement.upcase
+  end
+
+  def bye?(statement)
+    statement == 'BYE'
+  end
+end
 my_grandma = Grandma.new('Ginny')
 
-puts "What do you want to say to Grandma? "
+puts 'What do you want to say to Grandma? '
 until my_grandma.done_talking?
-  print "You say: " 
+  print 'You say: '
   you_ask = gets.strip
   my_grandma.count(you_ask)
   puts my_grandma.says(you_ask)
