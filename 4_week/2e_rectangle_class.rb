d91a387ecadedc9997333c245fed961762078f88
gabrie30 <jgabriels30@gmail.com>
2014-12-14 12:38:03 -0500

    Update 2e_rectangle_class.rb

---
+++ 4_week/2e_rectangle_class.rb
@@ -0,0 +1,20 @@
+class Rectangle
+ 
+  def initialize(length, width)
+    @area = length * width
+    @perimeter = 2 * ( length + width )
+  end
+ 
+  def area
+    @area 
+  end
+ 
+  def perimeter
+    @perimeter
+  end
+ 
+end
+ 
+r = Rectangle.new(23.45, 34.67) 
+puts r.area
+puts r.perimeter
