655bbe03375e6b8b5b7a35df876812e250204162
gabrie30 <jgabriels30@gmail.com>
2014-12-14 12:49:11 -0500

    Update 4e_swap_contents.rb

--- 4_week/4e_swap_contents.rb
+++ 4_week/4e_swap_contents.rb
@@ -1,4 +1,4 @@
-Exercise4. Write a Ruby program (call it p028swapcontents.rb) to do the following. Take two text files say A and B. The program should swap the contents of A and B. That is after the program is executed, A should contain B's contents and B should contains A's contents.
+#Exercise4. Write a Ruby program (call it p028swapcontents.rb) to do the following. Take two text files say A and B. The program should swap the contents of A and B. That is after the program is executed, A should contain B's contents and B should contains A's contents.
 
 file_a = File.open("a.txt", "r+")
 file_b = File.open("b.txt", "r+")
